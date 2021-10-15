cec172fd2f0b68f763529c4a1b2699bbff3fb79a
Arun <arunachalam944@gmail.com>
2021-10-14 23:34:53 -0700

    initial commit

---
+++ src/main/java/com/kubernetescore/KubernetescoreApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetescoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetescoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubernetescore/KubernetescoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetescore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetescoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
